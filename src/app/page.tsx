faa6111e4a573ebecd6c33a357d91e32d2d5337f
Thomas Sankara <thomassankara74@gmail.com>
2024-12-11 13:43:03 +0300

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="mx-auto max-w-4xl px-4 py-20">
+      <h1 className="text-3xl font-bold">
+        NextJs 15 TypeScript Starter with ShadCN, TailwindCSS and Prettier
+        Plugin For TailwindCSS
+      </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+      <div className="my-6">
+        <p className="text-sm leading-6 text-neutral-600">
+          This template uses the{" "}
+          <code className="rounded-lg bg-neutral-200 px-2 py-0.5 font-semibold">
+            src
+          </code>{" "}
+          folder and the App router. This template exists because I am learning
+          TypeScript and I needed a repository that is easy to clone, that has
+          all the dependencies I usually use in my projects.
+        </p>
+      </div>
+
+      <h2 className="mb-4 text-lg font-bold leading-6 text-neutral-600">
+        Why did I choose these?
+      </h2>
+
+      <ul className="space-y-4 text-sm leading-6 text-neutral-600">
+        <li>
+          ShadCN: An amazing component library{" "}
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://ui.shadcn.com"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
+            className="font-semibold text-blue-500 underline"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            https://ui.shadcn.com
           </a>
+          .
+        </li>
+        <li>
+          Prettier Plugin for TailwindCSS sorts Tailwind classes according to an
+          order agreed upon at Tailwind Labs:{" "}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/tailwindlabs/prettier-plugin-tailwindcss"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
+            className="font-semibold text-blue-500 underline"
           >
-            Read our docs
+            https://github.com/tailwindlabs/prettier-plugin-tailwindcss
           </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        </li>
+        <li>
+          TypeScript, because I'm learning TypeScript as more and more projects
+          that I love now use TypeScript as opposed to JavaScript.
+        </li>
+        <li>
+          The default font is set to Geist Sans in `globals.css` - the current
+          default font in NextJs 15.
+        </li>
+      </ul>
+      <small className="mt-4 block text-center">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://youtube.com/@tsbsankara"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
+          className="font-semibold text-blue-500 underline"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Find me here
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </small>
     </div>
   );
 }
